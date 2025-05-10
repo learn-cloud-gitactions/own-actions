a68d7c0623e33cd9c49c4df958d8820c5ac13ea1
Nivas-194 <nivas6302@gmail.com>
2025-05-10 09:27:44 +0530

    update entrypoint

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 echo "::debug::Running entrypoint.sh"
 echo "Hello $1"
+if [ -n "$HELLO" ]; then
+  echo "HELLO environment variable is set to: $HELLO"
+else
+  echo "HELLO environment variable is not set."
+fi
 echo "INPUT_WHO_TO_GREET: $INPUT_WHO_TO_GREET"
 echo "HELLO: $HELLO"
 time=$(date)
