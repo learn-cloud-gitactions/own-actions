33c6585d12cf4c3f54a610cd9af600e02d7bd28a
Nivas-194 <nivas6302@gmail.com>
2025-05-10 08:43:08 +0530

    updated actions

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -2,7 +2,7 @@
 echo "Hello, $1"
 echo "This is a test of the GitHub Actions Docker container."
 echo "::DEBUG::This is a debug message"
-echo "INPUT_WHO_TO_GREET=$INPUT_WHO_TO_GREET"
+echo "INPUT_WHO_TO_GREET=$INPUT_WHO-TO-GREET"
 echo "HELLO $HELLO"
 time=$(date)
 echo "::set-output name=time::$time"
